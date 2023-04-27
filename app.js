aec985bc9bc71117897f311898be91531336a804
sagar88826 <sagarmandal88826@gmail.com>
2023-04-27 21:27:11 +0530

    fixed routes problem

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ app.use(fileupload({
     useTempFiles: true
 }))
 app.use(express.static(path.join(__dirname, "build")))
-app.get("*", (req, res) => {
+app.get("/search", (req, res) => {
+    res.sendFile(path.join(__dirname, "./build/index.html"))
+})
+app.get("/mypost", (req, res) => {
+    res.sendFile(path.join(__dirname, "./build/index.html"))
+})
+app.get("/profile", (req, res) => {
     res.sendFile(path.join(__dirname, "./build/index.html"))
 })
 
